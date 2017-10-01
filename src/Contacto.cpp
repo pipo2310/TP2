30e945877e44de7cd759bcf875b9cabdd52b40bd
Alejandro2910 <32439030+Alejandro2910@users.noreply.github.com>
2017-10-01 17:21:55 -0600

    Update Contacto.cpp

--- src/Contacto.cpp
+++ src/Contacto.cpp
@@ -2,25 +2,26 @@
 #include <sstream>
 
 using namespace std;
-Contacto::Contacto(){}
+Contacto::Contacto(){
+    nombre="";
+    apellido="";
+    tipo="";
+    email="";
 
-Contacto::Contacto(std::string nombre, std::string apellido, std::string tipo, std::string email)
-{
-    this->nombre = nombre;
-    this->apellido = apellido;
-    this->tipo = tipo;
-    this->email = email;
 }
-void Contacto::setData(string n,string a,string t,string e)
+
+void Contacto::setData(string n,string a,string t,string e,string tC, int nu)
 {
     nombre=n;
     apellido=a;
     tipo=t;
     email=e;
+    numeros.setNumero(tC,nu);
 }
-string Contacto::getData()
+std::string Contacto::getData()
 {
-    stringstream os;
-    os<<"Nombre: "<<nombre<<"\nApellido: "<<apellido<<"\nTipo: "<<tipo<<"\nEmail: "<<email<<"\n"<<endl;
-    return os.str();
+    stringstream datos;
+    datos<<"Nombre: "<<nombre<<"\nApellido: "<<apellido<<"\nTipo: "<<tipo<<"\nEmail: "<<email<<"\n"<<endl;
+    datos<<numeros.showNumeros()<<"\n"<<endl;
+    return datos.str();
 }
