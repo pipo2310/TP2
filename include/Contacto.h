7d9475811a058d9c9d00940e8e19611457faa56e
Alejandro2910 <32439030+Alejandro2910@users.noreply.github.com>
2017-10-01 17:29:09 -0600

    Update Contacto.h

--- include/Contacto.h
+++ include/Contacto.h
@@ -4,23 +4,22 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include "VectorNumeros.h"
 
 using namespace std;
 
-//Propósito: ver dos formas para sobrecargar el operador de comparación
 class Contacto
 {
 public:
 	Contacto();
-	Contacto(string n, string a, string t, string e);
 	string getData();
-	void setData(string,string,string,string);
-	//bool operator<(const Estudiante& est) const;
+	void setData(string,string,string,string,string,int);
 
 	 string nombre;
      string apellido;
      string tipo;
      string email;
+     VectorNumeros numeros;
 
 };
 
